340a66729c08ce3c6da8c1d9db05b1f6b5cd52ee
Maheshbalan0507 <maheshbalan0507@gmail.com>
2024-05-23 13:19:26 +0530

    Employee 1 First Commit

---
+++ src/Addition.java
@@ -0,0 +1,12 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=10,b=5,c;
+		c=a+b;
+		System.out.println(c);
+
+
+	}
+
+}
---
+++ src/Multiplication.java
@@ -0,0 +1,11 @@
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		int a=10,b=5,c;
+		c=a*b;
+		System.out.println(c);
+
+	}
+
+}
